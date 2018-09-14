f9270d9cbd6973050390eeb018f0274d86542c09
narendar kumar <nkumar010896@gmail.com>
2018-09-14 17:42:09 +0530

    solved

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,34 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string s="";
+            foreach(char c in text)
+            {
+                int p=(int)c;
+                int k=p+(shiftKey%26);
+                if(p>=97 && p<123)
+                {
+                if(k>122)
+                {
+                    k=k-122+96;
+                }
+                s+=(char)k;
+                }
+             else if(p>=65 && p<91)
+                {
+                if(k>90)
+                {
+                    k=k-90+64;
+                }
+                    s+=(char)k;
+                }
+
+                else{
+                    s+=c;
+                }
+                
+            }
+            return s;
         }
     }
 }
